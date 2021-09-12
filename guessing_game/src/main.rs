26f1a0af688b10279fce74acd5228e4f33edb23b
tky-k <takuya.kawaguchi08@gmail.com>
2021-09-12 17:03:00 +0900

    feat: guess number game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("The secret number is {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please type a number.");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
